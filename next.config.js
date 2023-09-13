ff9b7fafd5f87589bec55f524afdd1ff679bb171
Gerald <darkobekoegerald2@gmail.com>
2023-09-13 08:24:20 -0700

    Added some stuff

--- next.config.js
+++ next.config.js
@@ -7,6 +7,12 @@ const nextConfig = {
                 hostname: "lh3.googleusercontent.com",
                 port: "",
                 pathname: "/a/**"
+            },
+            {
+                protocol: "http",
+                hostname: "127.0.0.1",
+                port: "8000",
+                pathname: "/media/**"
             }
         ]
     }
