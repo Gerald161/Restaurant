93d36e64710cdaac097457a99253db1301a2e0d9
Gerald <darkobekoegerald2@gmail.com>
2023-10-03 07:57:34 -0700

    Adding AI assistant

--- app/food/[slug]/components/FirstSection.jsx
+++ app/food/[slug]/components/FirstSection.jsx
@@ -3,14 +3,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styles from "./styles/firstSection.module.css";
 import styles2 from "./styles/SecondSection.module.css";
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { increment } from "../../../redux/counter";
 import { Slide, ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function FirstSection({data, slug}){
-  const count = useSelector((state) => state.counter.value);
-
   const dispatch = useDispatch();
 
   const allImages = data.images;
@@ -111,112 +109,149 @@ export default function FirstSection({data, slug}){
   },[])
 
   return (
-    <div onResize={(e)=>{checkWindowSize()}} className={styles.first_section}>
-      <div className={styles.first}>
-        <div className={styles.slider} ref={sliderRef}>
-          {
-            allImages.map((image,index)=>(
-              <div key={index} className={styles.img}>
-                <img loading="lazy" src={`http://127.0.0.1:8000/media/${image}`} alt="" />
-              </div>
-            ))
-          }
-        </div>
-      </div>
-
-      <div className={styles.extra_info}>
-      <ToastContainer/>
-
-        <h3>{data.name}</h3>
-
-        
-        {
-          allImages.length > 1 &&
-          <div className={
-            allImages.length > 2 ? styles.additional_images : styles.additional_images2
-          }>
+    <>
+      <div onResize={(e)=>{checkWindowSize()}} className={styles.first_section}>
+        <div className={styles.first}>
+          <div className={styles.slider} ref={sliderRef}>
             {
-              allImages.map((image, index)=>(
-                <div key={index} 
-                  onClick={(e)=>{slideImage(e, index)}}
-                  className={styles.additional_image}>
-                  <img className={index !== selectedIndex && styles.inactive}
-                   data-left={index * 70} data-left2={index * 100} 
-                   loading="lazy" src={`http://127.0.0.1:8000/media/${image}`} 
-                   alt="" />
+              allImages.map((image,index)=>(
+                <div key={index} className={styles.img}>
+                  <img loading="lazy" src={`http://127.0.0.1:8000/media/${image}`} alt="" />
                 </div>
               ))
             }
           </div>
-        }
+        </div>
 
-        <div className={styles2.second_section}>
-          <p><span className={styles2.price}>Price:</span><span>{data.price}</span>$</p>
+        <div className={styles.extra_info}>
+          <ToastContainer/>
 
-          <div>
-            <label htmlFor="amount">Amount:</label>
-            <input onChange={(e)=>{setAmount(e.target.value)}} value={amount} id="amount" type="number" min="1" name=""/>
+          <h3>{data.name}</h3>
+
+          {
+            allImages.length > 1 &&
+            <div className={
+              allImages.length > 2 ? styles.additional_images : styles.additional_images2
+            }>
+              {
+                allImages.map((image, index)=>(
+                  <div key={index} 
+                    onClick={(e)=>{slideImage(e, index)}}
+                    className={styles.additional_image}>
+                    <img className={index !== selectedIndex && styles.inactive}
+                    data-left={index * 70} data-left2={index * 100} 
+                    loading="lazy" src={`http://127.0.0.1:8000/media/${image}`} 
+                    alt="" />
+                  </div>
+                ))
+              }
+            </div>
+          }
+
+          <div className={styles2.second_section}>
+            <p><span className={styles2.price}>Price:</span><span>{data.price}</span>$</p>
+
+            <div>
+              <label htmlFor="amount">Amount:</label>
+                <input onChange={(e)=>{setAmount(e.target.value)}} value={amount} id="amount" type="number" min="1" name=""/>
+              </div>
+
+              <button onClick={()=>{addOrder(slug)}} className={styles2.order_button}>Add to Orders</button>
+            </div>
           </div>
 
-          <button onClick={()=>{addOrder(slug)}} className={styles2.order_button}>Add to Orders</button>
+          <style jsx global>
+          {
+              `
+              nav{
+                position: relative;
+                color: black;
+              }
+
+              nav div:nth-child(1) a div{
+                background: url("/logo2.png") no-repeat center/cover;
+              }
+
+              nav div:nth-child(3) a{
+                border: 1.5px solid black;
+              }
+      
+              nav div:nth-child(3) a:hover{
+                border: white;
+                color: white;
+                background: black;
+              }
+
+              nav div:nth-child(3) a:hover svg{
+                color: black;
+              }
+              
+              nav div:nth-child(3) div:nth-child(2) p{
+                color: white;
+                background: rgba(0,0,0,0.7);
+                padding: 7px 10px;
+                margin-right: 23px;
+                text-align: center;
+                bottom: -3px;
+              }
+
+              nav svg{
+                color: black;
+              }
+
+              a{
+                color: black;
+              }
+
+              .quick_nav{
+                display: none;
+              }
+
+              .main{
+                display: flex;
+                border-top: solid 1px #ccc;
+                margin-bottom: 5px;
+              }
+              `
+          }
+          </style>
+      </div>
+      
+      <div className={styles.chat_and_ai_button_container}>
+        <div className={styles.questionSection}>
+          <h2>AI Assistant</h2>
+          <div className={styles.message_content_section}>
+            <div className={styles.chat_window}>
+              <div className={styles.output}>
+                <div className={styles.profile_container}>
+                  <div className={styles.profile_pic}></div>
+                  <p>: Da Killa</p>
+                </div>
+                <div className={styles.profile_container}>
+                  <div className={styles.profile_pic2}></div>
+                  <p>: Sup?</p>
+                </div>
+                <div className={styles.profile_container}>
+                  <div className={styles.profile_pic}></div>
+                  <p>: Yo</p>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.message_compose_section}>
+              <input className={styles.message} type="text" placeholder={`Ask about ${slug.split("-")[0]}`} />
+              <button className={styles.send}>Send</button>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ai_button_container}>
+          <p>AI Chat</p>
+          <div className={styles.ai_chat_button}>
+            <img src="/logo2.png" alt="Chat logo" />
+          </div>
         </div>
       </div>
-
-      <style jsx global>
-      {
-          `
-          nav{
-            position: relative;
-            color: black;
-          }
-
-          nav div:nth-child(1) a div{
-            background: url("/logo2.png") no-repeat center/cover;
-          }
-
-          nav div:nth-child(3) a{
-            border: 1.5px solid black;
-          }
-  
-          nav div:nth-child(3) a:hover{
-            border: white;
-            color: white;
-            background: black;
-          }
-
-          nav div:nth-child(3) a:hover svg{
-            color: black;
-          }
-          
-          nav div:nth-child(3) div:nth-child(2) p{
-            color: white;
-            background: rgba(0,0,0,0.7);
-            padding: 7px 10px;
-            margin-right: 23px;
-            text-align: center;
-            bottom: -3px;
-          }
-
-          nav svg{
-            color: black;
-          }
-
-          a{
-            color: black;
-          }
-
-          .quick_nav{
-            display: none;
-          }
-
-          .main{
-            display: flex;
-            border-top: solid 1px #ccc;
-            margin-bottom: 5px;
-          }
-          `
-      }
-    </style>
-    </div>
+    </>
   )
 }
